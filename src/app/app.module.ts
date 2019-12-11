c0c4a1b2367b4e42628ae343cf5a08a0c77ee204
RichardPM99 <a3518110232@alumno.uttehuacan.edu.mx>
2019-12-10 20:19:30 -0600

    o

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,13 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './login/login.component';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent,LoginComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,FormsModule, ReactiveFormsModule],
   providers: [
     StatusBar,
     SplashScreen,
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,93 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { NavController, MenuController, ToastController, AlertController, LoadingController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss'],
+})
+export class LoginComponent implements OnInit {
+
+  public onLoginForm: FormGroup;
+
+  constructor(
+    public navCtrl: NavController,
+    public menuCtrl: MenuController,
+    public toastCtrl: ToastController,
+    public alertCtrl: AlertController,
+    public loadingCtrl: LoadingController,
+    private formBuilder: FormBuilder
+  ) { }
+
+  ionViewWillEnter() {
+    this.menuCtrl.enable(true);
+  }
+
+  ngOnInit() {
+
+    this.onLoginForm = this.formBuilder.group({
+      'email': [null, Validators.compose([
+        Validators.required
+      ])],
+      'password': [null, Validators.compose([
+        Validators.required
+      ])]
+    });
+  }
+
+  async forgotPass() {
+    const alert = await this.alertCtrl.create({
+      header: 'Forgot Password?',
+      message: 'Enter you email address to send a reset link password.',
+      inputs: [
+        {
+          name: 'email',
+          type: 'email',
+          placeholder: 'Email'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Confirm',
+          handler: async () => {
+            const loader = await this.loadingCtrl.create({
+              duration: 2000
+            });
+
+            loader.present();
+            loader.onWillDismiss().then(async l => {
+              const toast = await this.toastCtrl.create({
+                showCloseButton: true,
+                message: 'Email was sended successfully.',
+                duration: 3000,
+                position: 'bottom'
+              });
+
+              toast.present();
+            });
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
+
+  // // //
+  goToRegister() {
+    this.navCtrl.navigateRoot('/login');
+  }
+
+  goToHome() {
+    this.navCtrl.navigateRoot('/home');
+  }
+
+}
